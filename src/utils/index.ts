ac5d5495d7d841c1493f23a3c4f250eb68ca4079
zack <ljq180505@163.com>
2025-10-16 17:22:29 +0800

    delete code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,3 @@
-export const normalizeToDate = (ts: number) => {
-  const d = new Date(ts);
-  return new Date(
-    d.getFullYear(),
-    d.getMonth(),
-    d.getDate(),
-    0,
-    0,
-    0,
-    0
-  ).getTime();
-};
-
 export const monthShort = [
   "Jan",
   "Feb",
